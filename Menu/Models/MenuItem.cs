69fa81f6a2dcb2ddc87baa0d7f5abf9ffde6685e
blaise594 <danielbrogers@gmail.com>
2018-02-21 13:33:58 -0500

    first commit

---
+++ Menu/Models/MenuItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Menu.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public double Price { get; set; }
+        public bool IsNew { get; set; } = true;
+        public DateTime DateAdded {get; set; }
+    }
+    public enum Category
+    {
+        Appetizer,
+        Entree,
+        Dessert
+    }
+    
+}
